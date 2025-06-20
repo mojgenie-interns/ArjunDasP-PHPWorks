6d85d705ec494c75c22a6ee91fd053de5faa4691
Arjun Das P <arjundaspalappilli@gmail.com>
2025-06-20 11:34:46 +0530

    Added foreach-loop.php

---
+++ foreach-loop.php
@@ -0,0 +1,14 @@
+<?php
+    $week=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    foreach($week as $i)
+    {
+        echo "$i"."\n";
+    }
+
+    echo "\n";
+    $interns=["Int 1"=>"Mariya","Int 2"=>"Athira","Int 3"=>"Arjun"];
+    foreach($interns as $x=>$y)
+    {
+        echo "$x: $y\t";
+    }
+?>
