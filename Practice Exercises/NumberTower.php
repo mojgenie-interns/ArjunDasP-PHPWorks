8c4dfaa0b828e8954ea8e35b9e53c516da3db004
Arjun Das P <arjundaspalappilli@gmail.com>
2025-07-15 17:39:04 +0530

    Number Tower

---
+++ Practice Exercises/NumberTower.php
@@ -0,0 +1,35 @@
+<?php
+class NumberTower
+{
+    private $levels;
+
+    public function __construct()
+    {
+        $this->levels = (int)readline("Enter the number of levels: ");
+        $this->printTower();
+    }
+
+    private function printTower()
+    {
+        for ($i = 1; $i <= $this->levels; $i++) {
+            // Print leading spaces
+            for ($j = 1; $j <= $this->levels - $i; $j++) {
+                echo " ";
+            }
+
+            // Print ascending numbers
+            for ($j = 1; $j <= $i; $j++) {
+                echo $j;
+            }
+
+            // Print descending numbers
+            for ($j = $i - 1; $j >= 1; $j--) {
+                echo $j;
+            }
+
+            echo "\n";
+        }
+    }
+}
+
+new NumberTower();
